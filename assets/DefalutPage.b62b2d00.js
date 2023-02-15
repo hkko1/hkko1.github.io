ed97628edde3c3b2cbaba69bf189d5f4437d8636
hkko1 <hyekyung.ko@telus.com>
2023-02-15 14:25:11 -0800

    rebuild to use router

---
+++ assets/DefalutPage.b62b2d00.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.4d51ac22.js";import{_ as t,I as a,M as o,K as r,Q as s}from"./index.f9bad8e1.js";import"./render.1c6a37f1.js";const c={};function p(d,f){return a(),o(e,{style:{"padding-top":"60px","padding-left":"60px"},class:"text-h5"},{default:r(()=>[s(" Welcome! Make your todo Lists.")]),_:1})}var l=t(c,[["render",p]]);export{l as default};
